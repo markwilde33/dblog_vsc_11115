4539ea9fba0754c6c07de8127b2178ee09ff925c
markwilde33 <markwilde29@gmail.com>
2018-11-08 19:26:20 +0000

    Removed env.py

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "SECRET_KEY", "'g!!j+r)%j-%t2plba9#oz9svwxk2^3x!u63t__!@7@10do!p+g'")
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgres://dvmywbguwluitp:719cb20681fdcf622bf28d62d659c7d7e70c95f5500755061ef97d1c68942d2b@ec2-54-217-216-149.eu-west-1.compute.amazonaws.com:5432/dksaetcpduno0")
